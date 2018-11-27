606b1f36b152ce23ba0dc32adc66d16cbd2be4d6
陈新坤 <2023295451@qq.com>
2018-11-27 11:39:08 +0800

    IR_V0.6

--- test.ts
+++ test.ts
@@ -1,1 +1,13 @@
-// tests go here; this will not be compiled when this package is used as a library
+Mbit_IR.onPressEvent(RemoteButton.Up, function () {
+    serial.writeLine("12345689")
+})
+serial.redirect(
+SerialPin.P8,
+SerialPin.P12,
+BaudRate.BaudRate115200
+)
+Mbit_IR.init(Pins.P5)
+basic.forever(function () {
+    basic.pause(2000)
+    serial.writeLine("hello")
+})
